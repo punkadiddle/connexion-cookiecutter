a4e049c0c1905af4931231c5dc70c60190f329b6
punkadiddle <author@email.com>
2018-03-30 21:03:46 +0200

    fixes

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/notapi/hello.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/notapi/hello.py
@@ -7,4 +7,3 @@ helloPage = Blueprint('hello', __name__, template_folder='')
 @helloPage.route("/<page>")
 def hello(page):
     return "<html><body>Hello World!</body></html>"
-    
--- {{cookiecutter.project_slug}}/tests/__init__.py
+++ {{cookiecutter.project_slug}}/tests/__init__.py
@@ -4,4 +4,4 @@ from {{cookiecutter.project_slug}}.app import app
 # Get Flask app
 flaskApp = main({})
 flaskApp.testing = True
-client = flaskApp.test_client(
+client = flaskApp.test_client()
