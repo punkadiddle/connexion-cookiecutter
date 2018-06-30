a1b40e4b2279e6f93a08e4ed76b6a140d8dfb7a2
punkadiddle <2260848+punkadiddle@users.noreply.github.com>
2018-06-30 23:41:08 +0200

    distribution

--- {{cookiecutter.project_slug}}/ci/common/tasks/python-prep-push.sh
+++ {{cookiecutter.project_slug}}/ci/common/tasks/python-prep-push.sh
@@ -4,8 +4,23 @@ CHECKOUT_DIR='project-git'
 VERSION="$(cat "${BASE_DIR}/version/version")"
 source "${CHECKOUT_DIR}/ci/common/scripts/lib.sh"
 
+export PIP_CACHE_DIR="${PWD}/pip-cache"
+export SETUPTOOLS_SCM_PRETEND_VERSION
+
+# inject version since it is not published to git yet
+SETUPTOOLS_SCM_PRETEND_VERSION=$(tr -d '-' <<< "${VERSION}")
 echo "$VERSION" > "${CHECKOUT_DIR}/.version"
 
-ls -al "${CHECKOUT_DIR}"
+cd "${CHECKOUT_DIR}" || exit 1
 
+python -m pip install setuptools wheel || exit 1
+python setup.py egg_info
+result=$?
+
+#pkgRoot="$(find "${CHECKOUT_DIR}/src/" -maxdepth 2 -type f -name '__init__.py' | head -n 1)"
+#if [[ $? -eq 0 ]]; then
+#    echo "$VERSION" >  "$(dirname "$pkgRoot")/.version"
+#fi
+
+mv "${BASE_DIR}/${CHECKOUT_FOLDER}"/{,.[!.]}* "${BASE_DIR}/project-dist/"
 exit 0
